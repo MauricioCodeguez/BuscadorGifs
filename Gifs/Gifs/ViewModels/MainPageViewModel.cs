2cb97c37e4731c37bffa1137ace0356bee55b8b4
Mauricio Codeguez <mauricioscodeguez@outlook.com>
2019-06-18 00:18:59 -0300

    first commit

--- Gifs/Gifs/ViewModels/MainPageViewModel.cs
+++ Gifs/Gifs/ViewModels/MainPageViewModel.cs
@@ -13,7 +13,7 @@ namespace Gifs.ViewModels
         public MainPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
-            Title = "Main Page";
+            Title = "Gifs";
         }
     }
 }
